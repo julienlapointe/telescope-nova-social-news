35a1153be48a1ac4f408e008a442de5bb4d9f36c
Julien LaPointe <email@julienlapointe.com>
2017-04-07 01:04:56 -0400

    switched from post.createdAt (for admins) to post.postedAt (for everyone)

--- packages/my-custom-package/lib/components/TimeRemaining.jsx
+++ packages/my-custom-package/lib/components/TimeRemaining.jsx
@@ -5,12 +5,60 @@
 import React, { PropTypes, Component } from 'react';
 
 // create component
-export const TimeRemaining = (props) => (
-	<div>
-		{props.createdAt.toString()}
-	</div>
-)
+// export const TimeRemaining = (props) => (
+// export const TimeRemaining = (props) => (
+// 	<div>
+// 		{props.createdAt.toString()}
+// 	</div>
+// )
+
+
+export const TimeRemaining = (props) => {
+	const currentTime = new Date();
+	const timeRemaining = new Date();
+	timeRemaining.setTime(currentTime.getTime()-props.postedAt.getTime() );
+	return(
+		<div>
+			{timeRemaining.toString()}
+		</div>
+	)
+}
+
 
 // export component
 // any files that *import* the file TimeRemaining.js, will get the "TimeRemaining" component by *default*
+// export default TimeRemaining;
+
+
+
+
+// create component
+// var TimeRemaining = React.createClass({
+// 	getInitialState: function() {
+// 		return {
+// 			secondsRemaining: 0
+// 		};
+// 	},
+// 	tick: function() {
+// 		this.setState({secondsRemaining: this.state.secondsRemaining - 1});
+// 		if (this.state.secondsRemaining <= 0) {
+// 			clearInterval(this.interval);
+// 		}
+// 	},
+// 	componentDidMount: function() {
+// 		this.setState({ secondsRemaining: this.props.secondsRemaining });
+// 		this.interval = setInterval(this.tick, 1000);
+// 	},
+// 	componentWillUnmount: function() {
+// 		clearInterval(this.interval);
+// 	},
+// 	render: function() {
+// 		return (
+// 			<div>Seconds Remaining: {this.state.secondsRemaining.toString()}</div>
+// 		);
+// 	}
+// });
+
+// make this module's properties and functions (above) available to other files
+// module.exports = TimeRemaining;
 // export default TimeRemaining;
